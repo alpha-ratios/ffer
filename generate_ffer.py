d938e48040ecd92b322c105e9fa831d0720cb4d4
Scott Rogowski <scottrogowski@Scotts-MacBook-Pro.local>
2021-01-04 12:55:34 -0700

    remove stock from training set for significantly better results. Add fastmap

--- generate_ffer.py
+++ generate_ffer.py
@@ -3,7 +3,6 @@
 import argparse
 import csv
 import logging
-from collections import defaultdict
 from datetime import datetime
 from typing import List, Dict
 
@@ -14,6 +13,7 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MaxAbsScaler
 from xgboost import XGBRegressor
+import fastmap
 
 
 SCRIPT_DESCRIPTION = "Calculate the market cap estimate, price estimate, and FFER given a " \
@@ -53,7 +53,6 @@ ListOfStrs = List[str]
 ListOfDicts = List[dict]
 
 
-
 def split_df_x_y(df: pd.DataFrame, x_dimensions: ListOfStrs) -> (pd.DataFrame, IntByStrDict):
     """
     Get X, and Y dataframes from the input dataframe. Also return a row mapping dict
@@ -61,6 +60,7 @@ def split_df_x_y(df: pd.DataFrame, x_dimensions: ListOfStrs) -> (pd.DataFrame, I
     row_idx_by_ticker = {df['ticker'].iloc[i]: i for i in range(len(df))}
     df_x = df[x_dimensions]
     df_y = df[['marketCap', ]]
+
     return df_x, df_y, row_idx_by_ticker
 
 
@@ -76,38 +76,22 @@ def scale_dfs(df_x: pd.DataFrame, df_y: pd.DataFrame) -> (np.ndarray, np.ndarray
     return np_x, np_y, scalerx, scalery
 
 
-def make_estimates(model: XGBRegressor, np_x: np.ndarray, scalery: MaxAbsScaler,
-                   row_idx_by_ticker: IntByStrDict) -> ListOfDicts:
+def train_and_estimate_once(np_x: np.ndarray, np_y: np.ndarray, stock_np_x: np.ndarray,
+                            num_cols: int, scalery: MaxAbsScaler, rand_state: int) -> float:
     """
-    Given the model and the input array, and the y-scaler, make estimates for every x row
+    Train an XGBoost market-cap estimator using the entire market less one stock
+    and then make a prediction using that estimator for that stock.
+
+    Importanly, this has the rand_state which will slightly tweak the training.
+    The return value of this function is averaged over multiple rand_states
     """
-    np_estimate = model.predict(np_x)
-    np_estimate = scalery.inverse_transform(np_estimate.reshape(-1, 1))
 
-    raw_estimates = []
-    for ticker, idx in row_idx_by_ticker.items():
-        row = {"ticker": ticker, "estimate_market_cap": np_estimate[idx][0]}
-        raw_estimates.append(row)
-
-    return raw_estimates
-
-
-def train_and_estimate_once(df_x: pd.DataFrame, df_y: pd.DataFrame, row_idx_by_ticker: IntByStrDict,
-                            rand_state: int) -> (float, ListOfDicts):
-    """
-    Given the x, and y dataframes and the random state, split and train.
-    Return the r^2 and the raw estimates.
-    """
-    np_x, np_y, _, scalery = scale_dfs(df_x, df_y)
-    x_train, x_test, y_train, y_test = train_test_split(np_x, np_y, test_size=0.1,
-                                                        random_state=rand_state)
-    num_cols = len(df_x.columns)
-    model = XGBRegressor(max_depth=num_cols//4, learning_rate=.5, n_estimators=20, subsample=.8)
-    model.fit(x_train, y_train)
-    y_estimate = model.predict(x_test)
-    score = r2_score(y_test, y_estimate)
-    raw_estimates = make_estimates(model, np_x, scalery, row_idx_by_ticker)
-    return score, raw_estimates
+    model = XGBRegressor(max_depth=num_cols // 4, learning_rate=.5,
+                         n_estimators=20, subsample=.8, random_state=rand_state)
+    model.fit(np_x, np_y)
+    scaled_pred = model.predict(stock_np_x)
+    real_pred = scalery.inverse_transform(scaled_pred.reshape(-1, 1))
+    return float(real_pred[0][0])
 
 
 def log_estimates(estimates: ListOfDicts, log_all=False) -> None:
@@ -126,7 +110,7 @@ def log_estimates(estimates: ListOfDicts, log_all=False) -> None:
         logging.debug("%s actual:%.2f estimate:%.2f ffer:%.2f",
                       row["ticker"], row["actual_price"], row["estimate_price"], row["ffer"])
     logging.debug("...")
-    for row in estimates[len(estimates)//2-2:len(estimates)//2+2]:
+    for row in estimates[len(estimates) // 2-2:len(estimates)//2+2]:
         logging.debug("%s actual:%.2f estimate:%.2f ffer:%.2f",
                       row["ticker"], row["actual_price"], row["estimate_price"], row["ffer"])
     logging.debug("...")
@@ -135,10 +119,36 @@ def log_estimates(estimates: ListOfDicts, log_all=False) -> None:
                       row["ticker"], row["actual_price"], row["estimate_price"], row["ffer"])
 
 
-def generate_ffers_for_day(df: pd.DataFrame, dt=None, rounds=100) -> ListOfDicts:
+def calculate_overall_r2(df: pd.DataFrame, rounds: int) -> None:
+    """
+    Before we do any FFER calculations, an ensemble of models with different random
+    splits is trained from the whole market.
+    The r^2 of each model (probably 100) are then averaged together.
+    This function exists as a sanity check. We should assume high (>.9) r^2s
+    """
+    df_x, df_y, _ = split_df_x_y(df, X_DIMENSIONS)
+    num_cols = len(df_x.columns)
+    np_x, np_y, _, scalery = scale_dfs(df_x, df_y)
+
+    scores = []
+    for rand_state in range(rounds):
+        x_train, x_test, y_train, y_test = train_test_split(np_x, np_y, test_size=0.1,
+                                                            random_state=rand_state)
+        model = XGBRegressor(max_depth=num_cols // 4, learning_rate=.5,
+                             n_estimators=20, subsample=.8)
+        model.fit(np_x, np_y)
+        y_estimate = model.predict(x_test)
+        scores.append(r2_score(y_test, y_estimate))
+
+    logging.info("Avg r^2 over %d rows between %d different splits was %.2f",
+                 len(df), rounds, sum(scores) / rounds)
+
+
+def generate_ffers_for_day(df: pd.DataFrame, dt=None, single_ticker=None, rounds=None) -> ListOfDicts:
     """
     Given the input_csv, and the number of round, train <rounds> times and bag the results.
     """
+
     if dt:
         logging.info(f"Training {rounds} rounds of {len(df)} stocks for {dt}...")
     else:
@@ -149,37 +159,35 @@ def generate_ffers_for_day(df: pd.DataFrame, dt=None, rounds=100) -> ListOfDicts
         return []
 
     dt_str = dt and dt.strftime("%Y-%m-%d")
-    df_x, df_y, row_idx_by_ticker = split_df_x_y(df, X_DIMENSIONS)
+    df = df[(df.date == dt_str)]
 
-    all_scores = []
-    raw_estimates_by_ticker = defaultdict(list)
+    calculate_overall_r2(df, rounds)
 
-    def train_and_log(rand_state):
-        score, raw_estimates = train_and_estimate_once(df_x, df_y, row_idx_by_ticker, rand_state)
-        logging.debug("%d rand_state. %.2f score", rand_state, score)
-        return score, raw_estimates
-
-    for score, raw_estimates in map(train_and_log, range(rounds)):
-        if score < 0:
-            continue
-        all_scores.append(score)
-        for pred in raw_estimates:
-            raw_estimates_by_ticker[pred["ticker"]].append(pred["estimate_market_cap"])
-
-    avg_score = sum(all_scores) / len(all_scores)
-    logging.info("Mean training r^2: %.2f (%d positive rounds)", avg_score, len(all_scores))
+    raw_estimate_by_ticker = {}
+    if single_ticker:
+        single_ticker = single_ticker.upper()
+        estimate = generate_ffe_for_ticker(single_ticker, df, rounds)
+        market_cap = float(df[(df.ticker == single_ticker)]['marketCap'])
+        logging.info("FFER for %r: %.4f", single_ticker, market_cap / estimate)
+        raw_estimate_by_ticker[single_ticker] = estimate
+    else:
+        def _gfds(ticker):
+            return ticker, generate_ffe_for_ticker(ticker, df=df, rounds=rounds)
+        for ticker, estimate in fastmap.fastmap(_gfds, df['ticker']):
+            market_cap = float(df[(df.ticker == ticker)]['marketCap'])
+            logging.info("FFER for %r: %.4f", ticker, market_cap / estimate)
+            raw_estimate_by_ticker[ticker] = estimate
 
     estimates = []
-    for ticker, preds in raw_estimates_by_ticker.items():
-        row_idx = row_idx_by_ticker[ticker]
-        df_row = df.iloc[row_idx]
+    for ticker, pred in raw_estimate_by_ticker.items():
+        df_row = df[(df.ticker == ticker)].to_dict(orient='records')[0]
         row = {}
         if dt_str:
             row["date"] = dt_str
         row["ticker"] = ticker
         row["num_shares"] = df_row["shares"]
         row["actual_market_cap"] = df_row['marketCap']
-        row["estimate_market_cap"] = sum(preds) / len(preds)
+        row["estimate_market_cap"] = pred
         row["actual_price"] = df_row["price"]
         row["estimate_price"] = row["estimate_market_cap"] / row["num_shares"]
         row["ffer"] = row["actual_price"] / row["estimate_price"]
@@ -187,11 +195,35 @@ def generate_ffers_for_day(df: pd.DataFrame, dt=None, rounds=100) -> ListOfDicts
             row[col] = df_row[col]
         estimates.append(row)
 
-    df_estimate = pd.DataFrame([p["estimate_market_cap"] for p in estimates], columns=["marketCap"])
-    agg_score = r2_score(df_y, df_estimate)
-    logging.info("Aggregate bagged r^2: %.2f%%", agg_score * 100)  # TODO this should not be negative...
-
     return sorted(estimates, key=lambda p: p["ffer"])
+
+
+def generate_ffe_for_ticker(ticker: str, df: pd.DataFrame, rounds: int) -> dict:
+    """
+    Generate the FFE for a single ticker.
+
+    Steps:
+    - The dataset is split into x (16 dimensions) and y (market cap)
+    - The ticker-in-question is removed from the training dataset.
+    - Several models (probably 100) are then trained and the models used to
+      make a market cap estimate of the stock. These estimates are averaged and
+      that is our FFE
+    """
+    df_x, df_y, row_idx_by_ticker = split_df_x_y(df, X_DIMENSIONS)
+    ticker_idx = row_idx_by_ticker[ticker]
+
+    np_x, np_y, _, scalery = scale_dfs(df_x, df_y)
+    ticker_np_x = np_x[ticker_idx: ticker_idx + 1]
+    np_x = np.delete(np_x, ticker_idx, 0)
+    np_y = np.delete(np_y, ticker_idx, 0)
+
+    num_cols = len(df_x.columns)
+
+    estimates = []
+    for rand_state in range(rounds):
+        estimates.append(train_and_estimate_once(np_x, np_y, ticker_np_x,
+                                                 num_cols, scalery, rand_state))
+    return sum(estimates) / rounds
 
 
 def write_csv(estimates: ListOfDicts, output_csv: str) -> None:
@@ -204,7 +236,8 @@ def write_csv(estimates: ListOfDicts, output_csv: str) -> None:
     logging.info(f"Wrote estimates as {output_csv}")
 
 
-def generate_ffers_for_files(input_csvs: ListOfStrs, start_date: str, end_date: str,
+def generate_ffers_for_files(input_csvs: ListOfStrs, single_ticker: str,
+                             start_date: str, end_date: str,
                              rounds: int, drop_incomplete_stocks: bool) -> ListOfDicts:
     logging.info(f"Processing {len(input_csvs)} input file(s)")
     dfs = []
@@ -228,7 +261,7 @@ def generate_ffers_for_files(input_csvs: ListOfStrs, start_date: str, end_date: 
 
     if 'date' not in all_df:
         logging.info("No date column. Training on entire set")
-        return generate_ffers_for_day(all_df, rounds=rounds)
+        return generate_ffers_for_day(all_df, single_ticker=single_ticker, rounds=rounds)
 
     if start_date:
         all_df = all_df[all_df['date'] >= start_date]
@@ -245,14 +278,23 @@ def generate_ffers_for_files(input_csvs: ListOfStrs, start_date: str, end_date: 
     logging.info(f"Processing {len(df_by_date)} dates")
     estimates = []
     for dt, df in sorted(df_by_date.items()):
-        estimates += generate_ffers_for_day(df, dt=dt, rounds=rounds)
+        estimates += generate_ffers_for_day(df, dt=dt, single_ticker=single_ticker, rounds=rounds)
     return estimates
 
 
-def generate_ffer(input_csvs: ListOfStrs, output_csv="ffer.csv", start_date=None,
-                  end_date=None, rounds=100, drop_incomplete_stocks=False, verbose=False) -> None:
+def generate_ffer(input_csvs: ListOfStrs, output_csv="ffer.csv", fastmap_config=None,
+                  single_ticker=None, start_date=None, end_date=None, rounds=None,
+                  drop_incomplete_stocks=False, verbose=False) -> None:
     """Given a csv file, make estimates, log them, and write them to a CSV"""
-    estimates = generate_ffers_for_files(input_csvs, start_date, end_date,
+
+    if fastmap_config:
+        fastmap.global_init(config=fastmap_config,
+                            max_cloud_workers=10)
+    else:
+        fastmap.global_init(exec_policy='LOCAL',
+                            max_cloud_workers=10)
+
+    estimates = generate_ffers_for_files(input_csvs, single_ticker, start_date, end_date,
                                          rounds, drop_incomplete_stocks)
     if verbose:
         log_estimates(estimates, log_all=True)
@@ -268,6 +310,11 @@ if __name__ == "__main__":
     parser.add_argument("--output-csv", default="ffer.csv",
                         help="Filename of the csv output. Each row will be a stock. Columns will "
                              "include estimate_market_cap, estimate_price, and ffer.")
+    parser.add_argument("--fastmap-config",
+                        help="Location of fastmap config file. If omitted, fastmap "
+                             "will run locally.")
+    parser.add_argument("--ticker",
+                        help="Train for specific. If omitted, train for all tickers.")
     parser.add_argument("--start-date",
                         help="Limit the data processed to on-or-after the YYYY-MM-DD date. "
                              "Only effective if input_csvs have a date column. "
@@ -276,7 +323,7 @@ if __name__ == "__main__":
                         help="Limit the data processed to before the YYYY-MM-DD date. "
                              "Only effective if input_csvs have a date column. "
                              "Default is to process all data.")
-    parser.add_argument("--training-rounds", default=100,
+    parser.add_argument("--training-rounds", default=50,
                         help="Iterations of psuedo-randomly spliting into test/train sets and "
                              "re-training. Results are later combined via bagged-ensemble.")
     parser.add_argument("--drop-incomplete-stocks", action="store_true",
@@ -297,5 +344,6 @@ if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s %(asctime)s: %(message)s", datefmt="%H:%M:%S",
                         level=level)
 
-    generate_ffer(args.input_csvs, args.output_csv, args.start_date, args.end_date,
-                  int(args.training_rounds), args.drop_incomplete_stocks, args.verbose)
+    generate_ffer(args.input_csvs, args.output_csv, args.fastmap_config, args.ticker,
+                  args.start_date, args.end_date, int(args.training_rounds),
+                  args.drop_incomplete_stocks, args.verbose)
